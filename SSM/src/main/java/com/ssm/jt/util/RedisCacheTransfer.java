5afe4d2e4acaafae038b8dd6037ea82beb3f6fe0
jt <2039840744@qq.com>
2019-11-08 14:41:02 +0800

    整合好的

---
+++ SSM/src/main/java/com/ssm/jt/util/RedisCacheTransfer.java
@@ -0,0 +1,12 @@
+package com.ssm.jt.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+
+public class RedisCacheTransfer {
+    @Autowired
+    public void setRedisTemplate(RedisTemplate redisTemplate) {
+        RedisCache.setRedisTemplate(redisTemplate);
+    }
+}
+
---
+++ SSM/src/main/java/com/ssm/jt/util/StringUtil.java
@@ -0,0 +1,9 @@
+package com.ssm.jt.util;
+
+public class StringUtil {
+
+    public static  String  toLikeStr(String str) {
+        return  "%"+str+"%";
+    }
+
+}
